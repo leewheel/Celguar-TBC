6bb682c116dfc611406cd98ab803638038d5a49a
celguar <celguar@gmail.com>
2021-05-23 02:02:44 +0300

    BG fixes
    BG: Eye of the Storm fix
    Fix flag respawn right after being picked up
    Fix multiple capture of the same base by the same team
    BG: Alterac Valley fix
    Fix scores not showing correct values
    Fix graveyard/tower states
    Fix fire not spawning after captian kill
    Fix mines not being neutral at start
    Fix Snowfall GY worldstates
    Fix worldstate after capture/destroy
    
    BG: Arathi Basin fix
    Fix graveyard at bases - teleport to graveyard near base with spirit healer instead of inside base if BG is running
    Fix BG id in battlemaster list
    Battleground: do not count spirit of redemption twice or self kill

--- src/game/BattleGround/BattleGroundEY.cpp
+++ src/game/BattleGround/BattleGroundEY.cpp
@@ -63,7 +63,7 @@ void BattleGroundEY::Update(uint32 diff)
 
             if (m_flagState == EY_FLAG_STATE_WAIT_RESPAWN)
                 RespawnFlagAtCenter(true);
-            else
+            else if (m_flagState == EY_FLAG_STATE_ON_GROUND)
                 RespawnDroppedFlag();
         }
         else
@@ -272,7 +272,8 @@ bool BattleGroundEY::HandleEvent(uint32 eventId, Object* source, Object* target)
             {
                 if (eyTowerEvents[i][j].eventEntry == eventId)
                 {
-                    ProcessCaptureEvent(go, i, eyTowerEvents[i][j].team, eyTowerEvents[i][j].worldState, eyTowerEvents[i][j].message);
+                    if (eyTowerEvents[i][j].team != m_towerOwner[i])
+                        ProcessCaptureEvent(go, i, eyTowerEvents[i][j].team, eyTowerEvents[i][j].worldState, eyTowerEvents[i][j].message);
 
                     // no need to iterate other events or towers
                     return false;
@@ -461,6 +462,9 @@ void BattleGroundEY::RespawnFlagAtCenter(bool wasCaptured)
 // Method that respawns dropped flag; called if nobody picks the dropped flag after 10 seconds
 void BattleGroundEY::RespawnDroppedFlag()
 {
+    if (m_flagState != EY_FLAG_STATE_ON_GROUND)
+        return;
+
     RespawnFlagAtCenter(false);
 
     if (GameObject* flag = GetBgMap()->GetGameObject(m_droppedFlagGuid))
