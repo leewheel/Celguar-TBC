bdd731113f6f151404dce82784ea18166f0dfeaf
mostlikely4r <sebastiaan.keek@gmail.com>
2022-11-28 11:23:39 +0100

    -Bot command: Call randombot command from guildchat

--- src/game/Chat/ChatHandler.cpp
+++ src/game/Chat/ChatHandler.cpp
@@ -408,7 +408,6 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
                     }
                 }
             }
-
             sRandomPlayerbotMgr.HandleCommand(type, msg, *_player, "", GetPlayer()->GetTeam(), lang);
 #endif
 
