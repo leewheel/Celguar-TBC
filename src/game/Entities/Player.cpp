c898f6506f9b03bd3aaac1970d76c2ed4edfc3ae
Niam5 <ebakkedahl@gmail.com>
2024-03-06 20:19:42 -0800

    Add new player OnUnequip hook

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -10742,14 +10742,20 @@ Item* Player::EquipItem(uint16 pos, Item* pItem, bool update)
 #ifdef BUILD_ELUNA
         // used by eluna
         if (Eluna* e = GetEluna())
-            e->OnEquip(this, pItem2, bag, slot);
+        {
+            e->OnEquip(this, pItem2, bag, slot); // This is depricated and will be removed in the future
+            e->OnItemEquip(this, pItem2, slot);
+        }
 #endif
         return pItem2;
     }
 #ifdef BUILD_ELUNA
     // used by eluna
     if (Eluna* e = GetEluna())
-        e->OnEquip(this, pItem, bag, slot);
+    {
+        e->OnEquip(this, pItem, bag, slot); // This is depricated and will be removed in the future
+        e->OnItemEquip(this, pItem, slot);
+    }
 #endif
     return pItem;
 }
@@ -10770,6 +10776,14 @@ void Player::QuickEquipItem(uint16 pos, Item* pItem)
             pItem->AddToWorld();
             pItem->SendCreateUpdateToPlayer(this);
         }
+
+#ifdef BUILD_ELUNA
+        if (Eluna* e = GetEluna())
+        {
+            e->OnEquip(this, pItem, (pos >> 8), slot); // This is depricated and will be removed in the future
+            e->OnItemEquip(this, pItem, slot);
+        }
+#endif
     }
 }
 
@@ -10882,6 +10896,11 @@ void Player::RemoveItem(uint8 bag, uint8 slot, bool update)
                     }
                     else if (slot == EQUIPMENT_SLOT_OFFHAND)
                         UpdateWeaponDependantStats(OFF_ATTACK);
+
+#ifdef BUILD_ELUNA
+                    if (Eluna* e = GetEluna())
+                        e->OnItemUnEquip(this, pItem, slot);
+#endif
                 }
             }
 
@@ -10982,10 +11001,6 @@ void Player::DestroyItem(uint8 bag, uint8 slot, bool update)
             ApplyItemOnStoreSpell(pItem, false);
 
         ItemRemovedQuestCheck(pItem->GetEntry(), pItem->GetCount());
-#ifdef BUILD_ELUNA
-        if (Eluna* e = GetEluna())
-            e->OnRemove(this, pItem);
-#endif
 
         if (bag == INVENTORY_SLOT_BAG_0)
         {
@@ -11018,6 +11033,11 @@ void Player::DestroyItem(uint8 bag, uint8 slot, bool update)
 
                 // equipment visual show
                 SetVisibleItemSlot(slot, nullptr);
+
+#ifdef BUILD_ELUNA
+                if (Eluna* e = GetEluna())
+                    e->OnItemUnEquip(this, pItem, slot);
+#endif
             }
 
             m_items[slot] = nullptr;
